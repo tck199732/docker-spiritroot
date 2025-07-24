b27843937839c81fcf3786490d739adedc066216
tck199732 <ctb8135@wmich.edu>
2025-07-24 14:14:08 -0400

    💚 set LD_LIBRARY_PATH to system lib path

--- install_eigen3.sh
+++ install_eigen3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}"
+if [ -z "${LD_LIBRARY_PATH:-}" ]; then
+    export LD_LIBRARY_PATH="/usr/local/lib:/usr/local/lib64"
+else
+    export LD_LIBRARY_PATH="/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH"
+fi
 tar zxf eigen-3.4.0.tar.gz
 cd eigen-3.4.0
 mkdir build
